df4ff46a56deeb5f1cb3b7fc26bbee0728573f23
Daniel Albino <daniel18albino@gmail.com>
2020-08-29 12:03:43 +0100

    init commit

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        PlayerWalk();
+    }
+
+    void PlayerWalk()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if(h > 0)
+        {
+            myBody.velocity = new Vector2(speed, myBody.velocity.y);
+        } else if(h < 0)
+        {
+            myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+        } else
+        {
+            myBody.velocity = new Vector2(0f, myBody.velocity.y);
+        }
+    }
+
+}// class
